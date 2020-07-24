005cfc26b5370e74f445eb24d2936db1776564eb
Chandrasekhar <chandrasekhar2494@gmail.com>
2020-07-24 12:33:57 +0530

    MAVEN_OOP

---
+++ MAVEN/src/epam/AbstractM.java
@@ -0,0 +1,8 @@
+package epam;
+
+
+public abstract class AbstractM {
+	public abstract int total_weight(int a[],int n);
+	public abstract int num_of_candy(int low,int high,int a[],int n);
+
+}
---
+++ MAVEN/src/epam/Choco.java
@@ -0,0 +1,23 @@
+package epam;
+public  class Choco extends AbstractM{
+	public int total_weight(int[] choc,int n)
+	{
+		int tot=0;
+		for(int i=0;i<n;i++)
+		{
+			tot+=choc[i];
+		}
+		return tot;
+	}
+	public int num_of_candy(int low,int high,int[] choc,int c)
+	{
+		int count=0;
+		for(int i=0;i<c;i++)
+		{
+			if(choc[i]>=low && choc[i]<=high)
+				count++;
+		}
+		return count;
+	}
+
+}
